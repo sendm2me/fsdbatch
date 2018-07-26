e32406e722eb618560f4e3d69e2b12e4018a7fde
sendm2me <sendm2me@gmail.com>
2018-07-26 12:00:52 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Emp CRUD Application';
+  title = 'Emp CRUD Application123';
 }
